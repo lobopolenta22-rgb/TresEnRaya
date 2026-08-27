a2357f29be856e15d6afc64d2d58933727683354
lobopolenta22-rgb <lobopolenta22@gmail.com>
2026-08-27 20:18:09 -0300

    primer commit

---
+++ app/src/main/java/bo/edu/uajms/cristianalejandro/a2026_tresenraya/MainActivity.kt
@@ -0,0 +1,13 @@
+package bo.edu.uajms.cristianalejandro.a2026_tresenraya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
